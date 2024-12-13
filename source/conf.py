e13ae6f1704462bc5022aadfc70689dc9d9f7315
jbrunto <brunton.jonathan@epa.gov>
2024-12-13 11:41:49 -0500

    trying rtd theme

--- source/conf.py
+++ source/conf.py
@@ -14,10 +14,11 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 import os, sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 
 #import pydata_sphinx_theme
-extensions = []
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -27,6 +28,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 #html_baseurl = 
