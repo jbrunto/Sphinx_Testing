5e7f86014bb3c8f0890e1d6ba7dc89e1c3511f1c
jbrunto <brunton.jonathan@epa.gov>
2024-12-13 11:30:01 -0500

    importing os, sys and abspath

--- source/conf.py
+++ source/conf.py
@@ -14,6 +14,8 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 import os, sys
+sys.path.insert(0, os.path.abspath('..'))
+
 import pydata_sphinx_theme
 extensions = ['pydata_sphinx_theme']
 
